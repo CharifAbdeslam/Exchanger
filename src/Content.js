329d49f2d15dd0f43a1b2277e113a7c15eb42c27
unknown <charifabdeslem@gmail.com>
2018-01-25 12:27:31 +0100

    Update

--- src/Content.js
+++ src/Content.js
@@ -5,7 +5,8 @@ class Content extends Component {
   constructor(props){
     super(props);
     this.state={
-    currentCoin : data.currentCoin,
+    currentCoinSe : data.currentCoinSe,
+    currentCoinRe : data.currentCoinRe,
     Coins: data.Coins,
     searchSend:"",
     searchRecieve:""
@@ -14,17 +15,22 @@ class Content extends Component {
 searchingSe(e){
   this.setState({searchSend:this.refs.searchingSend.value.toLowerCase()})
 }
+searchingRe(e){
+  this.setState({searchRecieve:this.refs.searchingRecieve.value.toLowerCase()})
+}
 setNewCoinSend(coin,icon){
-  this.setState({currentCoin:{crCoinY:icon,crTextY:coin}})
+  this.setState({currentCoinSe:{crCoinY:icon,crTextY:coin}})
+}
+setNewCoinRecieve(coin,icon){
+  this.setState({currentCoinRe:{crCoinG:icon,crTextG:coin}})
 }
   render() {
     const cr = this.state;
-    let cName = this.state.Coins;
+    let cNameSEND = this.state.Coins;
     let searchSend = this.state.searchSend;
-    cName = cName.filter(data =>{
+    cNameSEND = cNameSEND.filter(data =>{
       return data.Texts.toLowerCase().indexOf(searchSend) >=0
     }).map(data =>{
-
       return (  <div className="container clickab55987" key={data._Id} onClick={(e)=> this.setNewCoinSend(data.Texts,data.Coin)}>
                <div className="row">
                <div className="col-3 text-center coin56488896">{data.Texts}</div>
@@ -33,7 +39,21 @@ setNewCoinSend(coin,icon){
             </div>
             <hr></hr>
           </div>)
-    })
+    });
+    let cNameRECIEVE = this.state.Coins;
+    let searchRecieve = this.state.searchRecieve;
+    cNameRECIEVE = cNameRECIEVE.filter(data =>{
+      return data.Texts.toLowerCase().indexOf(searchRecieve) >=0
+    }).map(data =>{
+      return (  <div className="container clickab55987" key={data._Id} onClick={(e)=> this.setNewCoinRecieve(data.Texts,data.Coin)}>
+               <div className="row">
+               <div className="col-3 text-center coin56488896">{data.Texts}</div>
+               <div className="col-3 coin6549875"><span className={data.Coin}></span></div>
+               <div className="col-6 text669874">{data.Alt}</div>
+            </div>
+            <hr></hr>
+          </div>)
+    });
     return (<div className = "container" > <div className="header88997">
   <h1 className="text-white">Fast, Secure, Anonymous &
     <b>Cheap!</b>
@@ -49,12 +69,11 @@ setNewCoinSend(coin,icon){
         <input type="text" className="form-control text-right inp65645987"/>
         <div className="input-group-append">
           <a className="btn btn-secondary btnbord6565487" role="button"  data-toggle="dropdown">
-            <i className={cr.currentCoin.crCoinY}></i>
+            <i className={cr.currentCoinSe.crCoinY}></i>
             <span className="coin6549875">
-              {cr.currentCoin.crTextY}<i className="fa fa-angle-down" aria-hidden="true"></i>
+              {cr.currentCoinSe.crTextY}<i className="fa fa-angle-down" aria-hidden="true"></i>
             </span>
           </a>
-
           <div className="dropdown-menu get654877989">
             <div className="input-group">
               <div className="input-group-prepend">
@@ -65,7 +84,7 @@ setNewCoinSend(coin,icon){
               <input type="text" className="form-control ingetsearch" onChange={(e)=> this.searchingSe(e)} ref="searchingSend"/>
             </div>
             <div className="dropdown-divider"></div>
-             {cName}
+             {cNameSEND}
           </div>
         </div>
       </div>
@@ -79,15 +98,22 @@ setNewCoinSend(coin,icon){
         <input type="text" className="form-control text-right inp65645987"/>
         <div className="input-group-append">
           <a className="btn btn-secondary btnbord6565487" role="button" data-toggle="dropdown">
-            <i className={cr.currentCoin.crCoinG}></i>
+            <i className={cr.currentCoinRe.crCoinG}></i>
             <span className="coin6549875">
-              {cr.currentCoin.crTextG}<i className="fa fa-angle-down" aria-hidden="true"></i>
+              {cr.currentCoinRe.crTextG}<i className="fa fa-angle-down" aria-hidden="true"></i>
             </span>
           </a>
-          <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            <a className="dropdown-item">Action2</a>
-            <a className="dropdown-item">Another action2</a>
-            <a className="dropdown-item">Something else here2</a>
+          <div className="dropdown-menu get654877989">
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text bg-white pres659656">
+                  <i className="fa fa-search" aria-hidden="true"></i>
+                </span>
+              </div>
+              <input type="text" className="form-control ingetsearch" onChange={(e)=> this.searchingRe(e)} ref="searchingRecieve"/>
+            </div>
+            <div className="dropdown-divider"></div>
+             {cNameRECIEVE}
           </div>
         </div>
       </div>
