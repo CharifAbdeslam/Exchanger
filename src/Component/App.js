64d64a66bb5a92348b1944fbe5ee8f54a1121b44
Charif Abdeslam <charifabdeslem@gmail.com>
2018-01-29 20:56:38 +0100

    Footer update

--- src/Component/App.js
+++ src/Component/App.js
@@ -25,66 +25,61 @@ export default class App extends Component {
 
   }
   render() {
-    return (
-      <div>
-        <div className="App00236">
-          <Menu/> {/* navbar */}
-          <Content {...this.state}/> {/* exchange form */}
-          <div className="container-fluid sdssxs">
-            {/* alt coin price */}
-
-                  <div className="row crypto_wrapper" id="crypto_wrapper">
-                    <CoinsRate {...this.state} symbol="cc ETH" coinName="ethereum"/>
-                    <CoinsRate {...this.state} symbol="cc LTC" coinName="litecoin"/>
-                    <CoinsRate {...this.state} symbol="cc XMR" coinName="monero"/>
-                    <CoinsRate {...this.state} symbol="cc XRP" coinName="ripple"/>
-                    <CoinsRate {...this.state} symbol="cc BCH" coinName="bitcoin-cash"/>
-                    <CoinsRate {...this.state} symbol="cc STR" coinName="stellar"/>
-                    <CoinsRate {...this.state} symbol="cc NEO" coinName="neo"/>
-                    <CoinsRate {...this.state} symbol="cc XEM" coinName="nem"/>
-                    <CoinsRate {...this.state} symbol="cc DASH" coinName="dash"/>
-                    <CoinsRate {...this.state} symbol="cc ZEC" coinName="zcash"/>
-                    <CoinsRate {...this.state} symbol="cc STEEM" coinName="steem"/>
-                    <CoinsRate {...this.state} symbol="cc ADA" coinName="cardano"/>
-                    <CoinsRate {...this.state} symbol="cc BC" coinName="bitcoin-gold"/>
-                    <CoinsRate {...this.state} symbol="cc IOTA" coinName="iota"/>
-                    <CoinsRate {...this.state} symbol="cc STRAT" coinName="stratis"/>
-                    <CoinsRate {...this.state} symbol="cc QTUM" coinName="qtum"/>
-                    <CoinsRate {...this.state} symbol="cc LSK" coinName="lisk"/>
-                    <CoinsRate {...this.state} symbol="cc USDT" coinName="tether"/>
-                    <CoinsRate {...this.state} symbol="cc SC" coinName="siacoin"/>
-                    <CoinsRate {...this.state} symbol="cc BCN" coinName="bytecoin-bcn"/>
-                    <CoinsRate {...this.state} symbol="cc BTS" coinName="bitshares"/>
-                    <CoinsRate {...this.state} symbol="cc XVG" coinName="verge"/>
-                    <CoinsRate {...this.state} symbol="cc ETH" coinName="ethereum"/>
-                    <CoinsRate {...this.state} symbol="cc LTC" coinName="litecoin"/>
-                    <CoinsRate {...this.state} symbol="cc XMR" coinName="monero"/>
-                    <CoinsRate {...this.state} symbol="cc XRP" coinName="ripple"/>
-                    <CoinsRate {...this.state} symbol="cc BCH" coinName="bitcoin-cash"/>
-                    <CoinsRate {...this.state} symbol="cc STR" coinName="stellar"/>
-                    <CoinsRate {...this.state} symbol="cc NEO" coinName="neo"/>
-                    <CoinsRate {...this.state} symbol="cc XEM" coinName="nem"/>
-                    <CoinsRate {...this.state} symbol="cc DASH" coinName="dash"/>
-                    <CoinsRate {...this.state} symbol="cc ZEC" coinName="zcash"/>
-                    <CoinsRate {...this.state} symbol="cc STEEM" coinName="steem"/>
-                    <CoinsRate {...this.state} symbol="cc ADA" coinName="cardano"/>
-                    <CoinsRate {...this.state} symbol="cc BC" coinName="bitcoin-gold"/>
-                    <CoinsRate {...this.state} symbol="cc IOTA" coinName="iota"/>
-                    <CoinsRate {...this.state} symbol="cc STRAT" coinName="stratis"/>
-                    <CoinsRate {...this.state} symbol="cc QTUM" coinName="qtum"/>
-                    <CoinsRate {...this.state} symbol="cc LSK" coinName="lisk"/>
-                    <CoinsRate {...this.state} symbol="cc USDT" coinName="tether"/>
-                    <CoinsRate {...this.state} symbol="cc SC" coinName="siacoin"/>
-                    <CoinsRate {...this.state} symbol="cc BCN" coinName="bytecoin-bcn"/>
-                    <CoinsRate {...this.state} symbol="cc BTS" coinName="bitshares"/>
-                    <CoinsRate {...this.state} symbol="cc XVG" coinName="verge"/>
-
-                  </div>
-
+    return (<div>
+      <div className="App00236">
+        <Menu/> {/* navbar */}
+        <Content {...this.state}/> {/* exchange form */}
+        <div className="container-fluid sdssxs">
+          {/* alt coin price */}
+          <div className="row crypto_wrapper" id="crypto_wrapper">
+            <CoinsRate {...this.state} symbol="cc ETH" coinName="ethereum"/>
+            <CoinsRate {...this.state} symbol="cc LTC" coinName="litecoin"/>
+            <CoinsRate {...this.state} symbol="cc XMR" coinName="monero"/>
+            <CoinsRate {...this.state} symbol="cc XRP" coinName="ripple"/>
+            <CoinsRate {...this.state} symbol="cc BCH" coinName="bitcoin-cash"/>
+            <CoinsRate {...this.state} symbol="cc STR" coinName="stellar"/>
+            <CoinsRate {...this.state} symbol="cc NEO" coinName="neo"/>
+            <CoinsRate {...this.state} symbol="cc XEM" coinName="nem"/>
+            <CoinsRate {...this.state} symbol="cc DASH" coinName="dash"/>
+            <CoinsRate {...this.state} symbol="cc ZEC" coinName="zcash"/>
+            <CoinsRate {...this.state} symbol="cc STEEM" coinName="steem"/>
+            <CoinsRate {...this.state} symbol="cc ADA" coinName="cardano"/>
+            <CoinsRate {...this.state} symbol="cc BC" coinName="bitcoin-gold"/>
+            <CoinsRate {...this.state} symbol="cc IOTA" coinName="iota"/>
+            <CoinsRate {...this.state} symbol="cc STRAT" coinName="stratis"/>
+            <CoinsRate {...this.state} symbol="cc QTUM" coinName="qtum"/>
+            <CoinsRate {...this.state} symbol="cc LSK" coinName="lisk"/>
+            <CoinsRate {...this.state} symbol="cc USDT" coinName="tether"/>
+            <CoinsRate {...this.state} symbol="cc SC" coinName="siacoin"/>
+            <CoinsRate {...this.state} symbol="cc BCN" coinName="bytecoin-bcn"/>
+            <CoinsRate {...this.state} symbol="cc BTS" coinName="bitshares"/>
+            <CoinsRate {...this.state} symbol="cc XVG" coinName="verge"/>
+            <CoinsRate {...this.state} symbol="cc ETH" coinName="ethereum"/>
+            <CoinsRate {...this.state} symbol="cc LTC" coinName="litecoin"/>
+            <CoinsRate {...this.state} symbol="cc XMR" coinName="monero"/>
+            <CoinsRate {...this.state} symbol="cc XRP" coinName="ripple"/>
+            <CoinsRate {...this.state} symbol="cc BCH" coinName="bitcoin-cash"/>
+            <CoinsRate {...this.state} symbol="cc STR" coinName="stellar"/>
+            <CoinsRate {...this.state} symbol="cc NEO" coinName="neo"/>
+            <CoinsRate {...this.state} symbol="cc XEM" coinName="nem"/>
+            <CoinsRate {...this.state} symbol="cc DASH" coinName="dash"/>
+            <CoinsRate {...this.state} symbol="cc ZEC" coinName="zcash"/>
+            <CoinsRate {...this.state} symbol="cc STEEM" coinName="steem"/>
+            <CoinsRate {...this.state} symbol="cc ADA" coinName="cardano"/>
+            <CoinsRate {...this.state} symbol="cc BC" coinName="bitcoin-gold"/>
+            <CoinsRate {...this.state} symbol="cc IOTA" coinName="iota"/>
+            <CoinsRate {...this.state} symbol="cc STRAT" coinName="stratis"/>
+            <CoinsRate {...this.state} symbol="cc QTUM" coinName="qtum"/>
+            <CoinsRate {...this.state} symbol="cc LSK" coinName="lisk"/>
+            <CoinsRate {...this.state} symbol="cc USDT" coinName="tether"/>
+            <CoinsRate {...this.state} symbol="cc SC" coinName="siacoin"/>
+            <CoinsRate {...this.state} symbol="cc BCN" coinName="bytecoin-bcn"/>
+            <CoinsRate {...this.state} symbol="cc BTS" coinName="bitshares"/>
+            <CoinsRate {...this.state} symbol="cc XVG" coinName="verge"/>
+          </div>
         </div>
       </div>
-        <Footer/>{/* hero end */}
-      </div>
-    );
+      <Footer/>{/* hero end */}
+    </div>);
   }
 }
--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -16,30 +16,44 @@ class Footer extends Component {
         </div>
         <div className="container-fluid">
           <div className="row justify-content-around">
-            <div className="col-md-3 jumbotron text-center ftheaders bg-light">
+            <div className="col-md-2 jumbotron text-center ftheaders bg-light">
               <h1>
-                <i className="fa fa-clock-o fa-4x text-primary" aria-hidden="true"></i>
+                <i className="fa fa-clock-o fa-2x text-primary" aria-hidden="true"></i>
               </h1>
               <h6>FAST TRANSACTION</h6>
               <p className="lead">Transaction are done in one click and your fund will be sent the moment you confirm the transaction, see
                 <span className="text-primary"> HOW IT WORKS </span>
                 for more information.</p>
             </div>
-            <div className="col-md-3 jumbotron text-center ftheaders bg-light">
+            <div className="col-md-2 jumbotron text-center ftheaders bg-light">
               <h1>
-                <i className="fa fa-eye-slash fa-4x text-primary" aria-hidden="true"></i>
+                <i className="fa fa-user-secret fa-2x text-primary" aria-hidden="true"></i>
               </h1>
               <h6>ANONYMITY AND PRIVACY</h6>
               <p className="lead">We will never ask you or disclose your email , address or any other personal information.
                 <span className="text-primary"> ANONYMITY AND PRIVACY </span>
                 is what make us diffrent from other exchanges.</p>
             </div>
-            <div className="col-md-3 jumbotron text-center ftheaders bg-light">
+            <div className="col-md-2 jumbotron text-center ftheaders bg-light">
               <h1>
-                <i className="fa fa-money fa-4x text-primary" aria-hidden="true"></i>
+                <i className="fa fa-money fa-2x text-primary" aria-hidden="true"></i>
               </h1>
               <h6>LOW FEE</h6>
               <p className="lead">Other cryptocurrency trading platform charge you with withdrawal fees ,transaction and exchange. We have a reasonable and a <span className="text-primary"> low fee of 0.5% </span>.</p>
+            </div>
+            <div className="col-md-2 jumbotron text-center ftheaders bg-light">
+              <h1>
+                <i className="fa fa-shield fa-2x text-primary" aria-hidden="true"></i>
+              </h1>
+              <h6>SECURITY</h6>
+              <p className="lead">The security of your funds is our main priority,we perform a daily maintenance on the servers with our services your funds <span className="text-primary"> will never be lost or stolen. </span></p>
+            </div>
+            <div className="col-md-2 jumbotron text-center ftheaders bg-light">
+              <h1>
+                <i className="fa  fa-thumbs-o-up fa-2x text-primary" aria-hidden="true"></i>
+              </h1>
+              <h6>A GOOD EXCHANGE RATE</h6>
+              <p className="lead">The security of your funds is our main priority,we perform a daily maintenance on the servers with our services your funds <span className="text-primary"> will never be lost or stolen. </span></p>
             </div>
           </div>
         </div>
