e3d1ff910792d57558a93819698eab75a23ac883
Charif Abdeslam <charifabdeslem@gmail.com>
2018-02-03 21:29:34 +0100

    Update

--- src/Component/Exchange.js
+++ src/Component/Exchange.js
@@ -3,15 +3,19 @@ import React, { Component } from 'react';
 import QRCode from 'qrcode.react';
 import ReactCountdownClock from "react-countdown-clock";
 class Exchange extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      time : 0
+    }
+  }
   clicktest(e){
     console.log(this.props.amountSend);
     console.log(this.props.amountRecieve);
     console.log(this.props.currentCoinSe.crTextY);
     console.log(this.props.currentCoinRe.crTextG);
-  }
-  myCallback(){
-    console.log("countdown end");
-  }
+    this.setState({time:960})
+  }  
   render() {
     return (
       <div className="col-md-3">
@@ -19,29 +23,20 @@ class Exchange extends Component {
        <div className="modal fade" id="exchangeModal" tabIndex="-1" role="dialog">
   <div className="modal-dialog modal-dialog-centered">
     <div className="modal-content">
-
-
       <div className="modal-body">
         <p>Make sur to send the correct amount to the correct address or the transaction will be cancelled.</p>
         <hr></hr>
-
         <div>
           <div className="text-center qrcontainer">
             <p>Please send:<span className="text-success"> {this.props.amountSend}</span> <span>{this.props.currentCoinSe.crTextY}</span> to the address below:</p>
               <QRCode value="0x88A37155e840893d0F154f03EE4BeebB199313Ec" />
               <p className="text-secondary">0x88A37155e840893d0F154f03EE4BeebB199313Ec</p>
-              <div className="container">
-                <div className="row">
-                  <ReactCountdownClock seconds={280} color="#000" alpha={0.7} size={70} onComplete={this.myCallback}/>
-                </div>
-              </div>
-
-
+                  <ReactCountdownClock seconds={this.state.time} color="#0D3044" size={70}/>
           </div>
         </div>
       </div>
       <div className="modal-footer">
-        <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        <button type="button" className="btn btn-secondary" onClick={(e)=>{this.setState({time:0})}}data-dismiss="modal">Cancel</button>
         <button type="button" className="btn btn-primary btexch6659775">Confirm</button>
       </div>
     </div>
