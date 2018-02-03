06b5e9ec424093fcbf08bb7d659b236181ccef04
Charif Abdeslam <charifabdeslem@gmail.com>
2018-02-03 17:55:38 +0100

    Update

--- src/Component/Exchange.js
+++ src/Component/Exchange.js
@@ -1,13 +1,16 @@
 
 import React, { Component } from 'react';
-
-
+import QRCode from 'qrcode.react';
+import ReactCountdownClock from "react-countdown-clock";
 class Exchange extends Component {
   clicktest(e){
     console.log(this.props.amountSend);
     console.log(this.props.amountRecieve);
     console.log(this.props.currentCoinSe.crTextY);
     console.log(this.props.currentCoinRe.crTextG);
+  }
+  myCallback(){
+    console.log("countdown end");
   }
   render() {
     return (
@@ -19,10 +22,23 @@ class Exchange extends Component {
 
 
       <div className="modal-body">
-        <p>Make sur to send the correct amount to the correct address or the transaction will be cancelled</p>
+        <p>Make sur to send the correct amount to the correct address or the transaction will be cancelled.</p>
         <hr></hr>
-        <p>Please send:<span className="text-success"> {this.props.amountSend}</span> <span>{this.props.currentCoinSe.crTextY}</span></p>
-        <input type="text" class="form-control"/>
+
+        <div>
+          <div className="text-center qrcontainer">
+            <p>Please send:<span className="text-success"> {this.props.amountSend}</span> <span>{this.props.currentCoinSe.crTextY}</span> to the address below:</p>
+              <QRCode value="0x88A37155e840893d0F154f03EE4BeebB199313Ec" />
+              <p className="text-secondary">0x88A37155e840893d0F154f03EE4BeebB199313Ec</p>
+              <div className="container">
+                <div className="row">
+                  <ReactCountdownClock seconds={280} color="#000" alpha={0.7} size={70} onComplete={this.myCallback}/>
+                </div>
+              </div>
+
+
+          </div>
+        </div>
       </div>
       <div className="modal-footer">
         <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
